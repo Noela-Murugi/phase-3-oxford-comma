b1187892e2babf336e8711435f01d8f9a6d5baa4
Noela-Murugi <murugin254@gmail.com>
2022-10-23 11:29:08 +0300

    completed the assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.size==2
+    arr[-1]="and #{arr[-1]}"
+    arr.join(" ")
+  elsif arr.size>2
+    arr[-1]="and #{arr[-1]}"
+    arr.join(", ")
+  else
+    arr.join(" ")
+  end
 end
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
